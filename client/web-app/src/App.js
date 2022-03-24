f2edf38e9da14fd0c843bf5e47cc7c6bd78aeeb1
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-03-24 10:20:45 +0600

    Router Setup for Dashboard, Login & Register Pages

--- client/web-app/src/App.js
+++ client/web-app/src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <div>My App</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
