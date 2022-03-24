83e339b374b2bfda33c3d9535812a292d21f1d7c
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-03-24 12:55:08 +0600

    Navigation Links in Header

--- client/web-app/src/components/Header.jsx
+++ client/web-app/src/components/Header.jsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+
+// Icons
+import { FaSignInAlt, FaUser } from "react-icons/fa";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">
+          <h1>apply4me</h1>
+        </Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="/login" className="nav-link">
+            <FaSignInAlt /> Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/register" className="nav-link">
+            <FaUser /> Register
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
