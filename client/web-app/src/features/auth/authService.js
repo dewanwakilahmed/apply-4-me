54aba55e5ea97450856f5b1504933e0c488614ec
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-03-26 14:52:40 +0600

    Created Register User Reducer & Action

--- client/web-app/src/features/auth/authService.js
+++ client/web-app/src/features/auth/authService.js
@@ -2,6 +2,19 @@ import axios from "axios";
 
 const API_URL = "/api/user/";
 
-const authService = {};
+// Register User
+const registerUser = async (userRegistrationData) => {
+  const response = await axios.post(API_URL, userRegistrationData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+const authService = {
+  registerUser,
+};
 
 export default authService;
--- client/web-app/src/features/auth/authSlice.js
+++ client/web-app/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 // Get User from Local Storage
 const user = JSON.parse(localStorage.getItem("user"));
@@ -11,18 +12,51 @@ const initialState = {
   message: "",
 };
 
+// Register User
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (userRegistrationData, thunkAPI) => {
+    try {
+      return await authService.registerUser(userRegistrationData);
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     reset: (state) => {
       state.isLoading = false;
-      state.isErorr = false;
+      state.isError = false;
       state.isSuccess = false;
       state.message = "";
     },
   },
-  extraReducers: () => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.user;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      });
+  },
 });
 
 export const { reset } = authSlice.actions;
